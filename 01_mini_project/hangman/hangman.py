2e29061df11f8d69a41492b9cedf962967f2918b
Andrey <42832639+dmi3ev1987@users.noreply.github.com>
2026-05-19 20:34:57 +0300

    Hangman, complete game.

--- 01_mini_project/hangman/hangman.py
+++ 01_mini_project/hangman/hangman.py
@@ -102,20 +102,71 @@ def display_hangman(tries):
 
 def play(word):
     word = word.upper()
+
     word_completion = '_' * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
     tries = 6
+    count = 0
     print('Давайте играть в угадайку слов!')
-    print(display_hangman(tries))
-    print(word_completion)
+    print(word)
 
+    while not guessed:
+        print()
+        print(display_hangman(tries))
+        print(f'Осталось {tries} попыток.')
+        print(word_completion)
+        print('Отгодайте букву или слово целиком:', end='')
+        guess = input().upper()
+        if not guess.isalpha():
+            print('Нужно вводить только буквы или слово целиком.')
+            continue
+        if len(guess) == 1:
+            if guess in guessed_letters:
+                print('Вы уже вводили данную буква, попробуйте ещё раз.')
+                continue
+            guessed_letters.append(guess)
+            if guess in word:
+                for i in range(len(word)):
+                    if word[i] == guess:
+                        word_completion = (
+                            word_completion[:i]
+                            + guess
+                            + word_completion[i + 1 :]
+                        )
+                if word_completion == word:
+                    guessed = True
+            else:
+                tries -= 1
+        else:
+            if guess in guessed_words:
+                print('Вы уже вводили данное слово, попробуйте ещё раз.')
+                continue
+            guessed_words.append(guess)
+            if guess == word:
+                guessed = True
+            else:
+                tries -= 1
+
+        count += 1
+        if tries == 0:
+            print('Вы проиграли=(')
+            break
+
+    if guessed:
+        print(f'Ура! Вы угадали слово с {count} попытки!')
+
+
+def play_again():
+    if input('Чтобы сыграть еще раз введити "да":').lower() == 'да':
+        main()
+    print('Спасибо за игру, пока пока=)')
 
 
 def main():
-    word = get_word()
-    play(word)
+    play(get_word())
+    play_again()
 
 
 if __name__ == '__main__':
